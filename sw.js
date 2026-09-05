8a631f1ba3d5814f548165943501136edc443765
vanishavashisth-tech <231605981+vanishavashisth-tech@users.noreply.github.com>
2026-09-05 20:02:41 +0000

    chore: refresh issue dataset and UI cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gsoc-finder-20260509130802';
+const CACHE_NAME = 'gsoc-finder-20260905200241';
 
 // Basic caching for offline resilience
 self.addEventListener('install', (event) => {
